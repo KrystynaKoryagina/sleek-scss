0cddc8f092a2985b01eb1e4853e01932ba46ef8b
KrystynaKoryaginae <krystyna.koryagina@gmail.com>
2018-07-13 21:42:17 +0300

    create sign in, scrollbar

---
+++ app/js/main/init.js
@@ -0,0 +1,21 @@
+$( function() {
+
+    // init profile-tabs
+    if($('.profile-tabs').length) {
+        $( '.profile-tabs' ).tabs(); 
+    }
+
+    // init login-tabs
+    if($('.login-tabs').length) {
+        $( '.login-tabs' ).tabs(); 
+    }
+
+    // init scrollbar
+    if($('.scrollbar-inner').length) {
+        $( '.scrollbar-inner' ).scrollbar(); 
+    }
+} );
+
+
+
+
